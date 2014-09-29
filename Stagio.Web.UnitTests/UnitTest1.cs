1ee7f7d83755fcac64459dbfb032bc1b2375ff6d
Yannick Mazières <yannickmazieres@yahoo.ca>
2014-09-29 12:24:59 -0400

    Ajout référence System.Web.Mvc aux tests unitaires

--- Stagio.Web.UnitTests/UnitTest1.cs
+++ Stagio.Web.UnitTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stagio.Web.Controllers;
 
 namespace Stagio.Web.UnitTests
 {
@@ -9,6 +10,7 @@ namespace Stagio.Web.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+          
         }
     }
 }
