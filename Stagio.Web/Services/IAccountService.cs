dc7346f46ef963728d32f235bf75e4ff4b3dd3e4
MartBarbeau <barbeau622@gmail.com>
2014-10-08 11:09:45 -0400

    added interface for services

--- Stagio.Web/Services/IAccountService.cs
+++ Stagio.Web/Services/IAccountService.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Stagio.Domain.Application;
+using Stagio.Domain.Entities;
 
 namespace Stagio.Web.Services
 {
-    public class IAccountService
+    public interface IAccountService
     {
+        MayBe<ApplicationUser> ValidateUser(string email, string password);
+
+        string HashPassword(string password);
+
+        bool UserEmailExist(string email);
     }
 }
--- Stagio.Web/Services/IHttpContextService.cs
+++ Stagio.Web/Services/IHttpContextService.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 
 namespace Stagio.Web.Services
 {
-    public class IHttpContextService
+    public interface IHttpContextService
     {
+        int GetUserId();
+        void AuthenticationSignIn(ClaimsIdentity identity);
+        void AuthenticationSignOut();
     }
 }
