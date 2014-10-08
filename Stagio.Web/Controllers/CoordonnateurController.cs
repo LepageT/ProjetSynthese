a63b1beb44d0ab33c608173c63c22e58b7980a45
Thomas Lepage <thomas.lepage@hotmail.ca>
2014-10-08 19:47:35 -0400

    Test 2...

--- Stagio.Web/Controllers/CoordonnateurController.cs
+++ Stagio.Web/Controllers/CoordonnateurController.cs
@@ -19,6 +19,8 @@ namespace Stagio.Web.Controllers
         {
             _coordonnateurRepository = coordonnateurRepository;
             _invitationRepository = invitationRepository;
+
+
         }
         // GET: Coordonnateur
         public virtual ActionResult Index()
