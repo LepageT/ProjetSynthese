3226cff4b120d3ce613ebd3f7c816faf0be4b684
Thomas Lepage <thomas.lepage@hotmail.ca>
2014-10-20 10:46:02 -0400

    Merge correction

--- Stagio.Web/Mappers/ViewModelToEntityMappingProfile.cs
+++ Stagio.Web/Mappers/ViewModelToEntityMappingProfile.cs
@@ -15,7 +15,14 @@ namespace Stagio.Web.Mappers
             Mapper.CreateMap<ViewModels.Student.Edit, Student>()
                 .ForMember(dest => dest.FirstName, opt => opt.Ignore())
                 .ForMember(dest => dest.LastName, opt => opt.Ignore())
-                .ForMember(dest => dest.Matricule, opt => opt.Ignore());
+                .ForMember(dest => dest.Matricule, opt => opt.Ignore())
+                .ForMember(dest => dest.Activated, opt => opt.Ignore())
+                .IgnoreAllNonExisting();
+
+            
+            Mapper.CreateMap<ViewModels.Student.Create, Student>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .IgnoreAllNonExisting();
 
             Mapper.CreateMap<ViewModels.Coordonnateur.Create, Coordonnateur>()
                 .IgnoreAllNonExisting();
