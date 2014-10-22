b6bc74843b4e66768f2d67d62a212bb549a1ffe5
LaurieLavoie <laurie13avril@hotmail.com>
2014-10-22 15:12:06 -0400

    Modifications

--- Stagio.Web.UnitTests/ControllerTests/StudentTests/StudentControllerCreateListTests.cs
+++ Stagio.Web.UnitTests/ControllerTests/StudentTests/StudentControllerCreateListTests.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using NSubstitute.Core;
 using Stagio.Domain.Entities;
 using Ploeh.AutoFixture;
 using System;
@@ -17,52 +18,40 @@ namespace Stagio.Web.UnitTests.ControllerTests.StudentTests
         [TestMethod]
         public void createlist_action_should_render_default_view()
         {
-             _fixture.CreateMany<Student>().ToList();
             var result = studentController.CreateList() as ViewResult;
 
-             Assert.AreEqual("", result.ViewName);
+            Assert.AreEqual("", result.ViewName);
         }
 
 
         [TestMethod]
         public void createList_post_can_not_create_two_profils_with_two_same_matricule()
         {
-            var listStudent = _fixture.CreateMany<ListStudent>(3).ToList();
-            var studentInDb = new List<Student>();
-
-            foreach (var student in listStudent)
+            var listStudents = _fixture.CreateMany<ListStudent>(3).ToList();
+            var liststudent = new ListStudent
             {
-                studentInDb.Add(new Student
-                {
-                    Matricule = student.Matricule,
-                    FirstName = student.FirstName,
-                    LastName = student.LastName
-
-                });
-            }
-
-            studentController.TempData["listStudent"] = listStudent;
+                FirstName = "Bob",
+                LastName = "Patrick",
+                Matricule = listStudents[0].Matricule
+            };
             studentController.CreateListPost();
-            studentRepository.GetAll().Returns(studentInDb.AsQueryable());
+            listStudents.Add(liststudent);
+            studentController.TempData["listStudent"] = listStudents;
 
-
-            studentController.TempData["listStudent"] = listStudent;
             studentController.CreateListPost();
-            
-            foreach (var studentCreated in studentInDb)
-            {
-                StudentRepositoryAddMethodShouldHaveReceived(studentCreated);
-            }
+
+            studentRepository.DidNotReceive().Add(Arg.Is<Student>(x => x.FirstName == listStudents[3].FirstName));
+            studentRepository.DidNotReceive().Add(Arg.Is<Student>(x => x.LastName == listStudents[3].LastName));
         }
-        
+
 
         [TestMethod]
         public void createListPost_model_sould_not_be_valid_if_listStudent_null_redirect_default_view()
         {
-            
-            var studentViewModel = new List<Student>();
-            studentViewModel = null;
-            studentController.TempData["listStudent"] = studentViewModel;
+            var listStudents = new List<ListStudent>();
+            listStudents = null;
+            studentController.TempData["listStudent"] = listStudents;
+
             var result = studentController.CreateListPost() as RedirectToRouteResult;
             var action = result.RouteValues["Action"];
 
@@ -72,36 +61,36 @@ namespace Stagio.Web.UnitTests.ControllerTests.StudentTests
         [TestMethod]
         public void createlist_post_should_add_student_to_repository()
         {
-            var listStudent = _fixture.CreateMany<ListStudent>(3).ToList();
-            var studentInDb = new List<Student>();
-
-            foreach(var student in listStudent) 
+            var listStudents = _fixture.CreateMany<ListStudent>(3).ToList();
+            var studentsInDb = new List<Student>();
+            foreach (var student in listStudents)
             {
-                studentInDb.Add(new Student
+                studentsInDb.Add(new Student
                 {
-                   Matricule = student.Matricule,
-                   FirstName = student.FirstName,
-                   LastName = student.LastName
-                    
+                    Matricule = student.Matricule,
+                    FirstName = student.FirstName,
+                    LastName = student.LastName
+
                 });
             }
+            studentController.TempData["listStudent"] = listStudents;
 
-            studentController.TempData["listStudent"] = listStudent;
             studentController.CreateListPost();
-            studentRepository.GetAll().Returns(studentInDb.AsQueryable());
-            foreach (var studentCreated in studentInDb)
+            studentRepository.GetAll().Returns(studentsInDb.AsQueryable());
+
+            foreach (var studentCreated in studentsInDb)
             {
                 StudentRepositoryAddMethodShouldHaveReceived(studentCreated);
             }
         }
 
         [TestMethod]
-        public void createlist_post_should_return_default_view_when_modelState_is_not_valid()
+        public void createlist_post_should_return_upload_when_modelState_is_not_valid()
         {
-
             studentController.ModelState.AddModelError("Error", "Error");
-            var studentViewModel = _fixture.CreateMany<Student>().ToList();
-            studentController.TempData["listStudent"] = studentViewModel;
+            var listStudents = _fixture.CreateMany<ListStudent>().ToList();
+            studentController.TempData["listStudent"] = listStudents;
+
             var result = studentController.CreateListPost() as RedirectToRouteResult;
             var action = result.RouteValues["Action"];
 
@@ -111,13 +100,26 @@ namespace Stagio.Web.UnitTests.ControllerTests.StudentTests
         [TestMethod]
         public void createlist_post_should_redirect_to_student_resultList_on_success()
         {
-            var studentViewModel = _fixture.CreateMany<ListStudent>(3).ToList();
-            studentController.TempData["listStudent"] = studentViewModel;
+            var listStudents = _fixture.CreateMany<ListStudent>(3).ToList();
+            studentController.TempData["listStudent"] = listStudents;
+
             var result = studentController.CreateListPost() as RedirectToRouteResult;
             var action = result.RouteValues["Action"];
 
             action.ShouldBeEquivalentTo("ResultCreateList");
+        }
 
+
+        [TestMethod]
+        public void createlist_post_should_not_add_student_if_matricule_is_not_valid()
+        {
+            var listStudents = _fixture.CreateMany<ListStudent>(3).ToList();
+            listStudents[1].Matricule = 1234567098;
+
+            studentController.TempData["listStudent"] = listStudents;
+            studentController.CreateListPost();
+
+            studentRepository.DidNotReceive().Add(Arg.Is<Student>(x => x.Matricule == listStudents[1].Matricule));
         }
 
         private void StudentRepositoryAddMethodShouldHaveReceived(Student student)
@@ -128,5 +130,6 @@ namespace Stagio.Web.UnitTests.ControllerTests.StudentTests
             studentRepository.Received().Add(Arg.Is<Student>(x => x.Matricule == student.Matricule));
             studentRepository.Received().Add(Arg.Is<Student>(x => x.Password == student.Password));
         }
+
     }
 }
---
+++ Stagio.Web/ViewModels/Student/ResultCreateList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stagio.Web.ViewModels.Student
+{
+    public class ResultCreateList
+    {
+        public List<ListStudent> ListStudentAdded { get; set; }
+        public List<ListStudent> ListStudentNotAdded { get; set; }
+    }
+}
