3bc4bf4220bde14ef53d7cc3c04e74c01d9c06a9
Elise Carbonneau-Leclerc <elise654@hotmail.com>
2014-10-23 09:02:15 -0400

    Inviter un autre employé fonctionnel

---
+++ Stagio.Web.AcceptanceTests/ContactEnterpriseTests/ContactEnterpriseInviteTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace Stagio.Web.AcceptanceTests.ContactEnterpriseTests
+{
+    [TestClass]
+    public class ContactEnterpriseInviteTests : BaseTests
+    {
+        [TestMethod]
+        public void contact_enterprise_should_be_able_to_access_invite_another_contact_page()
+        {
+            _driver.Navigate().GoToUrl("http://thomarelau.local/ContactEnterprise/InviteContactEnterprise");
+
+            try
+            {
+                _driver.FindElement(By.Id("inviteContact-page"));
+            }
+            catch (NoSuchElementException)
+            {
+                Assert.Fail("Identifiant inviteContact-page non trouvé sur la page.");
+            }
+        }
+    }
+}
--- Stagio.Web/Controllers/ContactEnterpriseController.cs
+++ Stagio.Web/Controllers/ContactEnterpriseController.cs
@@ -6,16 +6,20 @@ using System.Web.Mvc;
 using AutoMapper;
 using Stagio.DataLayer;
 using Stagio.Domain.Entities;
+using Stagio.Web.Services;
+using Stagio.Web.Module.Strings.Email;
 
 namespace Stagio.Web.Controllers
 {
     public partial class ContactEnterpriseController : Controller
     {
         private readonly IEntityRepository<ContactEnterprise> _contactEnterpriseRepository;
+        private readonly IMailler _mailler;
 
-        public ContactEnterpriseController(IEntityRepository<ContactEnterprise> contactEnterpriseRepository)
+        public ContactEnterpriseController(IEntityRepository<ContactEnterprise> contactEnterpriseRepository, IMailler mailler)
         {
             _contactEnterpriseRepository = contactEnterpriseRepository;
+            _mailler = mailler;
         }
 
         // GET: Enterprise
@@ -134,20 +138,49 @@ namespace Stagio.Web.Controllers
             return View();
         }
 
-        // POST: Enterprise/Delete/5
         [HttpPost]
-        public virtual ActionResult InviteContactEnterprise(int id, FormCollection collection)
+        public virtual ActionResult InviteContactEnterprise(ViewModels.ContactEnterprise.Reactive createContactEnterpriseViewModel)
         {
-            try
-            {
-                // TODO: Add delete logic here
+           
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (createContactEnterpriseViewModel.Email != null)
             {
-                return View();
+                var contactEnterpriseToSendMessage = Mapper.Map<ContactEnterprise>(createContactEnterpriseViewModel);
+                string messageInvitation = Request.Form["Message"].ToString();
+
+                string messageText = generateURLInvitationContactEnterprise(contactEnterpriseToSendMessage);
+
+                if (messageInvitation != null)
+                {
+                    messageText += EmailEnterpriseResources.MessageHeader;
+                    messageText += messageInvitation;
+                }
+
+                if (
+                    !_mailler.SendEmail(contactEnterpriseToSendMessage.Email, EmailEnterpriseResources.InviteSubject,
+                        messageText))
+                {
+                    ModelState.AddModelError("Email", "Error");
+                    return View(InviteContactEnterprise());
+                }
+                return RedirectToAction(MVC.Coordinator.InviteContactEnterpriseConfirmation());
             }
+            return View(createContactEnterpriseViewModel);
+        }
+
+
+        private string generateURLInvitationContactEnterprise(ContactEnterprise contactEnterpriseToSendMessage)
+        {
+            string messageText = "Un employé de votre entreprise vous invite à vous inscrire au site Stagio: ";
+            string invitationUrl = "http://thomarelau.local/ContactEnterprise/Reactivate?Email=" +
+                                   contactEnterpriseToSendMessage.Email + "&EnterpriseName=" +
+                                   contactEnterpriseToSendMessage.EnterpriseName + "&FirstName=" +
+                                   contactEnterpriseToSendMessage.FirstName + "&LastName=" +
+                                   contactEnterpriseToSendMessage.LastName + "&Telephone=" +
+                                   contactEnterpriseToSendMessage.Telephone + "&Poste=" + contactEnterpriseToSendMessage.Poste;
+
+            messageText += invitationUrl;
+            return messageText;
         }
     }
 }
