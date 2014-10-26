973997686e3d5ac69c812662bb836e77ad48311c
LaurieLavoie <laurie13avril@hotmail.com>
2014-10-26 13:16:41 -0400

    Commit en ligne de commande

---
+++ Stagio.Web/ViewModels/Stage/Details.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+
+namespace Stagio.Web.ViewModels.Stage
+{
+    public class Details
+    {
+
+        [DisplayName("Date de l'offre:")]
+        public DateTime PublicationDate { get; set; }
+
+        //Maybe an enterprise entity must be created.
+        [DisplayName("Entreprise ou Organisation:")]
+        public String CompanyName { get; set; }
+        [DisplayName("Adresse:")]
+        public String Adresse { get; set; }
+
+
+        //Responsable
+        [DisplayName("Nom:")]
+        public String ResponsableToName { get; set; }
+        [DisplayName("Titre:")]
+        public String ResponsableToTitle { get; set; }
+        [DisplayName("Courriel:")]
+        public String ResponsableToEmail { get; set; }
+        [DisplayName("Téléphone:")]
+        public String ResponsableToPhone { get; set; }
+        [DisplayName("Poste:")]
+        public int? ResponsableToPoste { get; set; }
+
+        //Contact
+        [DisplayName("Nom:")]
+        public String ContactToName { get; set; }
+        [DisplayName("Titre:")]
+        public String ContactToTitle { get; set; }
+        [DisplayName("Courriel:")]
+        public String ContactToEmail { get; set; }
+        [DisplayName("Téléphone:")]
+        public String ContactToPhone { get; set; }
+        [DisplayName("Poste:")]
+        public int? ContactToPoste { get; set; }
+
+        [DisplayName("Description du projet pour le stage")]
+        public String StageDescription { get; set; }
+        [DisplayName("Environnement matériel et logiciel spécifique au projet")]
+        public String EnvironnementDescription { get; set; }
+        [DisplayName("Nombre de stagiaires:")]
+        public int NbrStagiaire { get; set; }
+
+        [DisplayName("Stagiaire si connu:")]
+        public string StagiaireIfKnew { get; set; }
+        [DisplayName("Nom:")]
+        //Submit to:
+        public String SubmitToName { get; set; }
+        [DisplayName("Titre:")]
+        public String SubmitToTitle { get; set; }
+        [DisplayName("Courriel:")]
+        public String SubmitToEmail { get; set; }
+        [DisplayName("Date limite pour soummettre une candidature:")]
+        public DateTime LimitDate { get; set; }
+    }
+}
