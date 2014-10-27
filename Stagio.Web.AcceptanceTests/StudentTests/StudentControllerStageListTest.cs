23499155b43b9d8c2af8a32c082b3b8372bae34d
MartBarbeau <barbeau622@gmail.com>
2014-10-27 08:53:37 -0400

    test done

---
+++ Stagio.Web.AcceptanceTests/StudentTests/StudentControllerStageListTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace Stagio.Web.AcceptanceTests.StudentTests
+{
+    [TestClass]
+    public class StudentControllerStageListTest : BaseTests
+    {
+        [TestMethod]
+        public void student_home_page_should_display_stages()
+        {
+            _driver.Navigate().GoToUrl("http://thomarelau.local/Student/StageList");
+            var countText = _driver.FindElement(By.Id("stages-count")).Text;
+            var stageCount =  int.Parse(countText.Split(' ')[0]);
+
+            Assert.IsTrue(stageCount > 0);
+        }
+    }
+}
