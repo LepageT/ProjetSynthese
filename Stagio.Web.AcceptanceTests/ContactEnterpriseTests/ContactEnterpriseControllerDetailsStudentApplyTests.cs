f784504dd8b286b457f985ba3c0e8f1e61cae697
LaurieLavoie <laurie13avril@hotmail.com>
2014-10-29 14:29:45 -0400

    Tests d'acceptations fait

---
+++ Stagio.Web.AcceptanceTests/ContactEnterpriseTests/ContactEnterpriseControllerDetailsStudentApplyTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace Stagio.Web.AcceptanceTests.ContactEnterpriseTests
+{
+    [TestClass]
+    public class ContactEnterpriseControllerDetailsStudentApplyTests : BaseTests
+    {
+        [TestMethod]
+        public void contactEnterprise_can_see_a_page_of_detail_apply()
+        {
+            _driver.Navigate().GoToUrl("http://thomarelau.local/ContactEnterprise/ListStage");
+            _driver.FindElement(By.Id("list-stages1")).Click();
+            _driver.FindElement(By.Id("list-student1")).Click();
+            try
+            {
+                _driver.FindElement(By.Id("details-apply-student"));
+            }
+            catch (NoSuchElementException)
+            {
+                Assert.Fail("Identifiant details-apply-student non trouvé sur la page.");
+            }
+        
+        }
+
+    }
+}
