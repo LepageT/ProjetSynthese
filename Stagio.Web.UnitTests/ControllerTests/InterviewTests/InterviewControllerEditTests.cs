a960b965680b4cf8dc9948e2255ee9fcdc2c7788
LaurieLavoie <laurie13avril@hotmail.com>
2014-11-06 09:27:10 -0500

    tests unitaires ne marchent pas

---
+++ Stagio.Web.UnitTests/ControllerTests/InterviewTests/InterviewControllerEditTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stagio.Web.UnitTests.ControllerTests.InterviewTests
+{
+    [TestClass]
+    public class InterviewControllerEditTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
