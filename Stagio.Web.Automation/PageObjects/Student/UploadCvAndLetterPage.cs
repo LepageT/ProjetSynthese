e829bdf6524174b778653926bf1374ad0f539159
LaurieLavoie <laurie13avril@hotmail.com>
2014-11-10 15:25:20 -0500

    Fin user story

---
+++ Stagio.Web.Automation/PageObjects/Student/UploadCvAndLetterPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenQA.Selenium;
+using Stagio.Web.Automation.Selenium;
+
+namespace Stagio.Web.Automation.PageObjects.Student
+{
+    public class UploadCvAndLetterPage 
+    {
+        public static bool IsDisplayed
+        {
+            get { return Driver.Instance.FindElement(By.Id("uploadCvLetter-page")) != null; }
+        }
+
+        public static void GoToByUrl()
+        {
+
+            Driver.Instance.Navigate().GoToUrl("http://thomarelau.local/Student/UploadCVLetter");
+        }
+
+        public static bool SelectFiles(string file1, string file2)
+        {
+            try
+            {
+
+                Driver.Instance.FindElement(By.Id("file1")).SendKeys(file1);
+                Driver.Instance.FindElement(By.Id("file2")).SendKeys(file2);
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            Driver.Instance.FindElement(By.Id("button-upload")).Click();
+            return true;
+        }
+    }
+}
