167b023eb4a0eb7a7b5630275d7d17b487225f0e
LaurieLavoie <laurie13avril@hotmail.com>
2014-11-17 08:44:25 -0500

    Fin user story

--- Stagio.Web.AcceptanceTests/ContactEnterpriseTests/ContactEnterpriseControllerDetailsStudentApplyTests.cs
+++ Stagio.Web.AcceptanceTests/ContactEnterpriseTests/ContactEnterpriseControllerDetailsStudentApplyTests.cs
@@ -7,6 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using Stagio.Web.Automation.PageObjects;
 using Stagio.Web.Automation.PageObjects.ContactEnterprise;
+using Stagio.Web.Automation.PageObjects.Student;
 
 namespace Stagio.Web.AcceptanceTests.ContactEnterpriseTests
 {
@@ -58,15 +59,22 @@ namespace Stagio.Web.AcceptanceTests.ContactEnterpriseTests
             Assert.IsTrue(DetailsStudentApplyContactEnterprisePage.ErrorDisplayed);
         }
 
-        [TestMethod]
-        public void contactEnterprise_should_download_files_isfiles_valid()
-        {
-            LoginPage.GoTo();
-            LoginPage.LoginAs(ContactEnterpriseUsername, ContactEnterprisePassword);
-            DetailsStudentApplyContactEnterprisePage.GoToByUrl();
-            DetailsStudentApplyContactEnterprisePage.DownloadPage();
-            Assert.IsTrue(DetailsStudentApplyContactEnterprisePage.ErrorDisplayed);
-        }
+        //[TestMethod]
+        //public void contactEnterprise_should_download_files_isfiles_valid()
+        //{
+
+        //    LoginPage.GoTo();
+        //    LoginPage.LoginAs(StudentUsername, StudentPassword);
+        //    ApplyStudentPage.GoToByUrl();
+        //    ApplyStudentPage.SelectFiles("C:\\dev\\abc.pdf", "C:\\dev\\abcdef.pdf");
+            
+        //    LoginPage.GoToByUrl();
+        //    LoginPage.LoginAs(ContactEnterpriseUsername, ContactEnterprisePassword);
+        //    DetailsStudentApplyContactEnterprisePage.GoToByUrl();
+        //    DetailsStudentApplyContactEnterprisePage.DownloadPage();
+        //    DetailsStudentApplyContactEnterprisePage.DownloadFile("C:\\dev\\abc.pdf");
+        //    Assert.IsFalse(DetailsStudentApplyContactEnterprisePage.ErrorDisplayed);
+        //}
 
     }
 }
