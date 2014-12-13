4d6afcd5ca014aac028bab728151702c64f94745
MartBarbeau <barbeau622@gmail.com>
2014-12-13 15:45:15 -0500

    test done!!!!

---
+++ Stagio.Web.AcceptanceTests/CoordinatorTests/CoordinatorControllerChangeSmtpOptionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stagio.Web.Automation.PageObjects;
+using Stagio.Web.Automation.PageObjects.Coordinator;
+
+namespace Stagio.Web.AcceptanceTests.CoordinatorTests
+{
+    [TestClass]
+    public class CoordinatorControllerChangeSmtpOptionsTests : BaseTests
+    {
+        private const string SERVER = "smtp.qc.ca";
+        private const string SMTP_PORT = "25";
+        private const string USERNAME = "";
+        private const string PASSWORD = "";
+        private const string EMAIL = "test@test.com";
+        [TestMethod]
+        public void Coordinator_should_be_able_to_change_smtp_options()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs(CoordonatorUsername, CoordonatorPassword);
+
+             ChangeSMTPOptionsPage.GoTo();
+            ChangeSMTPOptionsPage.FillFieldsAndSend(SERVER, SMTP_PORT, USERNAME, PASSWORD, EMAIL);
+
+            IndexCoordinatorPage.IsDisplayed.Should().BeTrue();
+        }
+    }
+}
---
+++ Stagio.Web.Automation/PageObjects/Coordinator/ChangeSMTPOptionsPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenQA.Selenium;
+using Stagio.Web.Automation.Selenium;
+
+namespace Stagio.Web.Automation.PageObjects.Coordinator
+{
+    public class ChangeSMTPOptionsPage
+    {
+        public static void GoTo()
+        {
+            Navigation.Coordinator.ChangeSmtpOptions.Select();
+        }
+
+        public static void FillFieldsAndSend(string server, string port, string username, string password, string email)
+        {
+            Driver.Instance.FindElement(By.Id("SmtpServer")).Clear();
+            Driver.Instance.FindElement(By.Id("SmtpServer")).SendKeys(server);
+            Driver.Instance.FindElement(By.Id("SmtpPort")).Clear();
+            Driver.Instance.FindElement(By.Id("SmtpPort")).SendKeys(port);
+            Driver.Instance.FindElement(By.Id("SmtpUsername")).Clear();
+            Driver.Instance.FindElement(By.Id("SmtpUsername")).SendKeys(username);
+            Driver.Instance.FindElement(By.Id("SmtpPassword")).Clear();
+            Driver.Instance.FindElement(By.Id("SmtpPassword")).SendKeys(password);
+            Driver.Instance.FindElement(By.Id("TestEmail")).Clear();
+            Driver.Instance.FindElement(By.Id("TestEmail")).SendKeys(email);
+            Driver.Instance.FindElement(By.Id("SaveSmtpOptions")).Click();
+        }
+
+    }
+}
